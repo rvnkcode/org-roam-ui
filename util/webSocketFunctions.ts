687cabc6587db12049cd342bb88957657645d088
rvnkcode <sykim.p508@gmail.com>
2025-10-11 17:50:57 +0900

    style: `yarn prettier`

--- util/webSocketFunctions.ts
+++ util/webSocketFunctions.ts
@@ -2,24 +2,28 @@ import { OrgRoamNode } from '../api'
 import ReconnectingWebSocket from 'reconnecting-websocket'
 
 export function sendMessageToEmacs(command: string, data: {}, webSocket: ReconnectingWebSocket) {
-    webSocket.send(JSON.stringify({ command: command, data: data }))
+  webSocket.send(JSON.stringify({ command: command, data: data }))
 }
 
 export function getOrgText(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
-    sendMessageToEmacs('getText', { id: node.id }, webSocket)
+  sendMessageToEmacs('getText', { id: node.id }, webSocket)
 }
 
 export function openNodeInEmacs(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
-    sendMessageToEmacs('open', { id: node.id }, webSocket)
+  sendMessageToEmacs('open', { id: node.id }, webSocket)
 }
 
 export function deleteNodeInEmacs(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
-    if (node.level !== 0) {
-        return
-    }
-    sendMessageToEmacs('delete', { id: node.id, file: node.file }, webSocket)
+  if (node.level !== 0) {
+    return
+  }
+  sendMessageToEmacs('delete', { id: node.id, file: node.file }, webSocket)
 }
 
 export function createNodeInEmacs(node: OrgRoamNode, webSocket: ReconnectingWebSocket) {
-    sendMessageToEmacs('create', { id: node.id, title: node.title, ref: node.properties.ROAM_REFS }, webSocket)
+  sendMessageToEmacs(
+    'create',
+    { id: node.id, title: node.title, ref: node.properties.ROAM_REFS },
+    webSocket,
+  )
 }
